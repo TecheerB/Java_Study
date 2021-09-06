855a3d6bbba51a85e3a103fb2e11359dbafc0429
Gnu-Kenny <qkr0677@gmail.com>
2021-09-06 23:56:09 +0900

    [210906] BOJ_2588_곱셈

---
+++ GnuPark/1주차/연산자/operator_2588.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class operator_2588 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        String b = sc.next();
+
+        for (int i = 2; i >= 0; i--) {
+            System.out.println(a * Integer.parseInt(String.valueOf(b.charAt(i))));
+        }
+        System.out.println(a * Integer.parseInt(String.valueOf(b)));
+    }
+}
