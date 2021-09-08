c5bf1432da5420b01c43b57db984043445fd44c9
Eunji <eunji980310@gmail.com>
2021-09-09 00:30:18 +0900

    [210909] BOJ_1330_두 수 비교하기

---
+++ EunjiShin/1주차/조건문과 반복문/1330.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class problem1330 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        System.out.println( (A == B) ? "==" : ( (A > B) ? ">" : "<") );
+
+    }
+}
