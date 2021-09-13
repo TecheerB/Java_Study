46b48266b016842ea38bc134bcef1fb39aa613b7
yjshin229 <78634177+yjshin229@users.noreply.github.com>
2021-09-12 21:22:52 -0400

    folder added

---
+++ YoungjinShin/1주차/prob10869.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class prob10869{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        sc.close();
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+
+    }
+}
