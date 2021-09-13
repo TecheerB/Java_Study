73ade9a583b034f3293c3680b55f1ad8e9506826
printSANO <86213086+printSANO@users.noreply.github.com>
2021-09-12 19:40:22 -0700

    [210913] 숫자의 개수

---
+++ RyanLee/1주차/개수.java
@@ -0,0 +1,35 @@
+// 백준 2577
+
+import java.util.Scanner;
+
+public class 개수 {
+        public static void main(String[] args) {
+            Scanner scan = new Scanner(System.in);
+            
+            int a;
+            int b;
+            int c;
+
+            a = scan.nextInt();
+            b = scan.nextInt();
+            c = scan.nextInt();
+
+            int abc = (a * b * c);
+            String str = Integer.toString(abc);
+
+            scan.close();
+
+            int length = String.valueOf(abc).length();
+
+            for (int x = 0; x <= 9; x++){ // 0 to 9
+                int count = 0;
+                String z = Integer.toString(x);
+                for (int y = 0; y < length; y++){ // length of abc
+                    if (str.charAt(y) == z.charAt(0)){
+                        count++;
+                    }
+                }
+                System.out.println(count);
+            }
+        }
+}
