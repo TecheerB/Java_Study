6951b7f86866e30db5fbe839ce09b26702470d25
Gnu-Kenny <qkr0677@gmail.com>
2021-10-10 22:45:33 +0900

    [211010] BOJ_3052_나머지

---
+++ GnuPark/5주차/lambda_stream_3052.java
@@ -0,0 +1,27 @@
+public class lambda_stream_3052 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[10];
+        int[] targetArr = new int[10];
+        int count = 0;
+
+        for (int i = 0; i < 10; i++) {
+            arr[i] = sc.nextInt();
+            targetArr[i] = arr[i] % 42;
+        }
+
+        for (int i = 0; i < 10; i++) {
+            int temp = 0;
+            for (int j = i + 1; j < 10; j++) {
+                if (targetArr[i] == targetArr[j]) {
+                    temp++;
+                }
+            }
+            if (temp == 0) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
